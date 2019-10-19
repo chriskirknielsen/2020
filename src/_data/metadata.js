841957c20efdc7bdf33b0a5098342086ae0a9cde
chriskirknielsen <chriskirknielsen@gmail.com>
2019-10-18 23:16:40 -0400

    Update metadata.

---
+++ src/_data/metadata.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "chriskirknielsen",
+  url: "https://ckn2020.netlify.com",
+  author: {
+    name: "Christopher Kirk-Nielsen",
+    email: "chriskirknielsen@gmail.com",
+    twitter: "https://twitter.com/ckirknielsen"
+  },
+  currentYear: (new Date()).getFullYear(),
+  languages: [
+    {
+      label: "English",
+      code: "en"
+    },
+    {
+      label: "Français",
+      code: "fr"
+    }
+  ]
+};
