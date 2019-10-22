4381a0ea425c41b128285bab505dde4184293f74
chriskirknielsen <chriskirknielsen@gmail.com>
2019-10-21 20:47:47 -0400

    Prevent compiling error.

--- .eleventy.js
+++ .eleventy.js
@@ -41,6 +41,8 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addFilter('sortNavLocale', function(collection, navSet) {
+    if (!Array.isArray(collection)) { return collection; }
+    
     // Neat solution found on https://stackoverflow.com/a/44063445/3624336
     let collectionSorted = collection.slice().sort(function(a, b){
       return navSet.indexOf(a.fileSlug) - navSet.indexOf(b.fileSlug);
