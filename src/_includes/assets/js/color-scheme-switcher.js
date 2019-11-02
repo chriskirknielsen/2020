59e6812fc82c6106b3d7718dcfd0a6bd6a1129ae
chriskirknielsen <chriskirknielsen@gmail.com>
2019-11-02 16:13:50 -0400

    Implement localStorage for colour scheme switcher.

--- src/_includes/assets/js/color-scheme-switcher.js
+++ src/_includes/assets/js/color-scheme-switcher.js
@@ -1,8 +1,31 @@
-document.addEventListener('click', function(e){
-    const schemeButton = e.target.closest('[data-scheme-switcher]');
+(function(){
+    const schemeStorageKey = 'colourScheme';
     const schemeOptions = ['dusk','dawn'];
-    if (!schemeButton) { return; }
-    const currentSchemeIndex = schemeOptions.findIndex(s => s === document.documentElement.dataset.scheme);
-    const newSchemeIndex = 1 - currentSchemeIndex;
-    document.documentElement.dataset.scheme = schemeOptions[newSchemeIndex];
-}, false);
+
+    const setScheme = function (scheme) {
+        if (!schemeOptions.includes(scheme)) {
+            scheme = schemeOptions[0];
+        }
+
+        window.localStorage.setItem(schemeStorageKey, scheme);
+        document.documentElement.dataset.scheme = scheme;
+    }
+
+    const getScheme = function () {
+        return window.localStorage.getItem(schemeStorageKey) || document.documentElement.dataset.scheme || schemeOptions[0];
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const initScheme = window.localStorage.getItem(schemeStorageKey);
+        setScheme(initScheme || null);
+    }, false);
+
+    document.addEventListener('click', function(e){
+        const schemeButton = e.target.closest('[data-scheme-switcher]');
+        if (!schemeButton) { return; }
+        const currentSchemeIndex = schemeOptions.findIndex(s => s === getScheme());
+        const newSchemeIndex = 1 - currentSchemeIndex;
+
+        setScheme(schemeOptions[newSchemeIndex]);
+    }, false);
+})();
