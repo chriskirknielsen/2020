dc498991be58c3938a3d754ba3763204d1b5c23b
chriskirknielsen <chriskirknielsen@gmail.com>
2019-11-03 14:07:16 -0500

    Move lang switcher (todo: popover).

---
+++ src/_data/icons.js
@@ -0,0 +1,19 @@
+module.exports = {
+'scheme': `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+    <defs><circle id="icon-scheme-circle" cx="12" cy="12" r="12"/></defs>
+    <clipPath id="icon-scheme-clip">
+        <use xlink:href="#icon-scheme-circle" overflow="visible"/>
+    </clipPath>
+    <g clip-path="url(#icon-scheme-clip)">
+        <path fill="currentColor" d="M0 19.67h24V21H0zM0 23h24v1H0zM0 13h24v2H0zM0 16.33h24V18H0zM0 0h24v12H0z"/>
+    </g>
+</svg>`,
+
+'lang': `<svg width="23" height="24" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg">
+    <g stroke="currentColor" fill="none" fill-rule="evenodd">
+        <path d="M22.5 12a11 11 0 11-22 0 11 11 0 0122 0zM22.5 12H.5M11.5 1v22"/>
+        <path d="M16.81 12c0 6.07-2.38 11-5.31 11-2.93 0-5.31-4.93-5.31-11S8.57 1 11.5 1c2.93 0 5.31 4.93 5.31 11z"/>
+        <path d="M19.12 19.92c-1.58-1.67-4.4-2.79-7.62-2.79-3.22 0-6.04 1.12-7.62 2.8M3.88 4.08c1.58 1.67 4.4 2.79 7.62 2.79 3.22 0 6.04-1.12 7.62-2.8"/>
+    </g>
+</svg>`,
+};
