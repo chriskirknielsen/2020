c7d6da72df00aecb646293a8cb4ad905a00449c3
chriskirknielsen <chriskirknielsen@gmail.com>
2019-11-09 15:58:14 -0500

    Improve gulp pipeline to watch for token updates.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const root = 'src'; // Root folder
 const gulp = require("gulp");
 //const sass = require("gulp-sass");
 const sass = require('gulp-dart-sass');
+const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
 
 gulp.task('css', function () {
     return gulp.src([
@@ -16,10 +18,18 @@ gulp.task('css', function () {
 });
 
 gulp.task("watch", function () {
+    gulp.watch('./'+root+'/_data/tokens.json', gulp.parallel('tokens'));
     gulp.watch('./'+root+'/_includes/assets/scss/**/*.scss', gulp.parallel('css'));
     //gulp.watch('./'+root+'/_includes/assets/js/**/*.js', gulp.parallel('js'));
 });
 
-gulp.task('build', gulp.parallel(
-    'css'
-));
+gulp.task('tokens', function (cb) {
+    const cmd = spawn('npx', ['json-to-scss', root+'/_data/tokens.json', root+'/_includes/assets/scss/settings/_tokens.scss'], {stdio: 'inherit'});
+    cmd.on('close', function (code) {
+        console.log('tokens exited with code ' + code);
+        cb(code);
+    });
+    return cmd;
+})
+
+gulp.task('build-css', gulp.series('tokens', 'css'));
