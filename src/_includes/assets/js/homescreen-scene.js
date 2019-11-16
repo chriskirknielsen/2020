d8347c406cc95d315513187b61653e5520094344
chriskirknielsen <chriskirknielsen@gmail.com>
2019-11-16 17:19:09 -0500

    Add fancy grid on homescreen.

--- src/_includes/assets/js/homescreen-scene.js
+++ src/_includes/assets/js/homescreen-scene.js
@@ -2,7 +2,7 @@ function getContext(width, height) {
     const canvas = document.getElementById('homescreen-scene');
     canvas.width = width;
     canvas.height = height;
-    canvas.style.background = '#000000';
+    canvas.style.background = '#46115b';
     return canvas.getContext('2d');
 }
 
@@ -31,21 +31,21 @@ function draw() {
     };
     const periods = {};
     const now = new Date();
-    for (t in speeds) periods[t] = (now % speeds[t]) / speeds[t];
+    for (t in speeds) { periods[t] = (now % speeds[t]) / speeds[t]; }
 
     const horizonSpacing = 400
     const horizonWobble = 3 * Math.sin(periods.sway * 2 * Math.PI);
-    const infinity = height / 2 + horizonWobble;
-    let lowerHorizon = infinity + horizonSpacing / 2 + horizonWobble + Math.sin(periods.zScroll * Math.PI * 2) * 100;
-    let upperHorizon = infinity - horizonSpacing / 2 + horizonWobble + Math.sin(periods.zScroll * Math.PI * 2) * 100;
+    const infinity = height / 2 + 0;//horizonWobble;
+    let lowerHorizon = infinity + horizonSpacing / 2; // + horizonWobble + Math.sin(periods.zScroll * Math.PI * 2) * 100;
+    let upperHorizon = infinity - horizonSpacing / 2; // + horizonWobble + Math.sin(periods.zScroll * Math.PI * 2) * 100;
 
     const MAX_HLINES = 100;
     for (let i = 0; i < MAX_HLINES; i++) {
         const dy = Math.pow(1.3, i + periods.vScroll);
 
         const upperY = upperHorizon + 1 - dy;
-        ctx.strokeStyle = `rgba(0, 127, 255, 1)`;
-        drawLine(ctx, 0, upperY, width, upperY);
+        // ctx.strokeStyle = `rgba(0, 127, 255, 1)`;
+        // drawLine(ctx, 0, upperY, width, upperY);
 
         const lowerY = lowerHorizon - 1 + dy;
         ctx.strokeStyle = `rgba(255, 0, 255, 1)`;
@@ -56,14 +56,14 @@ function draw() {
 
     const VLINES = 50;
     const HSPREAD = 1500;
-    const z = Math.sin(periods.zScroll * Math.PI * 2) * HSPREAD * 0.8;
+    const z = HSPREAD * 0.05; // * Math.sin(periods.zScroll * Math.PI * 2);
     for (let i = 0; i < VLINES; i++) {
         const xSep = width / VLINES;
-        const x = width * (i / VLINES) + Math.sin(periods.hScroll * Math.PI * 2) * xSep;
+        const x = width * (i / VLINES);// + Math.sin(periods.hScroll * Math.PI * 2) * xSep;
 
         const xSpreadUpper = ((HSPREAD + z) * 2 / width) * x - (HSPREAD + z);
-        ctx.strokeStyle = 'cyan';
-        drawLine(ctx, x + xSpreadUpper, 0, x, upperHorizon);
+        // ctx.strokeStyle = 'cyan';
+        // drawLine(ctx, x + xSpreadUpper, 0, x, upperHorizon);
 
         const xSpreadLower = ((HSPREAD - z) * 2 / width) * x - (HSPREAD - z);
         ctx.strokeStyle = 'red';
