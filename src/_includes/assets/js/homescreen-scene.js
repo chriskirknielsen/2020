5d6959e166ef8c8d9f3fae6999c3f17f814486b3
chriskirknielsen <chriskirknielsen@gmail.com>
2019-11-17 19:23:34 -0500

    Improve homescreen scene sun. Make vertical lines proportional to screen width.

--- src/_includes/assets/js/homescreen-scene.js
+++ src/_includes/assets/js/homescreen-scene.js
@@ -32,12 +32,6 @@
 
         ctx.clearRect(0, 0, width, height);
 
-        var gradient = ctx.createLinearGradient(0,height/4, 0,height/2);
-
-        // Add three color stops
-        gradient.addColorStop(0, electric);
-        gradient.addColorStop(1, hotpink);
-
         const speeds = {
             sway: 3000,
             hScroll: 7000,
@@ -53,7 +47,7 @@
         let lowerHorizon = infinity + 1; // + Math.sin(periods.zScroll * Math.PI * 2) * 100;
         let upperHorizon = infinity - horizonSpacing / 2; // + Math.sin(periods.zScroll * Math.PI * 2) * 100;
 
-        const MAX_HLINES = 100;
+        const MAX_HLINES = 50;
         for (let i = 0; i < MAX_HLINES; i++) {
             const dy = Math.pow(1.5, i + periods.vScroll);
 
@@ -64,7 +58,8 @@
             if (lowerY > height) break;
         }
 
-        const VLINES = 50;
+        let VLINES = Math.floor(width / 50);
+        if (VLINES % 2 !== 0) { VLINES++; }
         const HSPREAD = 3000;
         const z = HSPREAD * 0.05; // * Math.sin(periods.zScroll * Math.PI * 2);
         for (let i = 0; i < VLINES; i++) {
@@ -77,8 +72,13 @@
         }
 
         // Draw half-sun
+        let sunRadius = (width < height) ? width/4 : height/4;
+        let gradient = ctx.createLinearGradient(0, height/2-sunRadius, 0, height/2);
+        gradient.addColorStop(0, electric);
+        gradient.addColorStop(1, hotpink);
+
         ctx.beginPath();
-        ctx.arc(width/2, height/2, height/4, Math.PI, 0, false);
+        ctx.arc(width/2, height/2, sunRadius, Math.PI, 0, false);
         ctx.fillStyle = gradient;
         ctx.fill();
 
