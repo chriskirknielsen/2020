3e03105e6cd378d1221b90fd2d658025108f2948
chriskirknielsen <chriskirknielsen@gmail.com>
2019-11-29 20:07:38 -0500

    Update translation location for some components.

---
+++ src/_data/header.js
@@ -0,0 +1,8 @@
+module.exports = {
+    en: {
+        rssHint: 'Subscribe to chriskirknielsen\'s RSS feed'
+    },
+    fr: {
+        rssHint: 'S\'inscrire au flux RSS de chriskirknielsen'
+    }
+}
