f3dfefdeadfaa7e23767b6a347296f1015557627
chriskirknielsen <chriskirknielsen@gmail.com>
2020-02-19 21:24:40 -0500

    Change contact box styles

--- src/_data/utilities.js
+++ src/_data/utilities.js
@@ -1,4 +1,4 @@
 module.exports = { // Define a list of utility classes that are commonly used together to form a basic component/object
     "glowBoxLink": "u-fontBold h:u-c--grey-min h:u-bg--grey-max h:u-inner-glow--secondary-med h:text-shadow--currentBg h:u-textDecoration--none",
-    "glowBoxContact": "u-flex--grow-1 u-flex--basis-0 u-flow u-padding u-c--grey-min u-bg--grey-max u-inner-glow--secondary-med "
+    "glowBoxContact": "u-flex--grow-1 u-flex--basis-0 u-flow u-padding u-c--grey-min u-bg--grey-max u-border u-border--grey-med"
 }
