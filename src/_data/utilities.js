d9630e624894eab00d00ee97e860dc887d5c8348
chriskirknielsen <chriskirknielsen@gmail.com>
2020-03-10 19:17:47 -0400

    Change card styles

--- src/_data/utilities.js
+++ src/_data/utilities.js
@@ -8,8 +8,8 @@ module.exports = { // Define a list of utility classes that are commonly used to
     // Card component
     cardFlex: "u-flex--grow-1 u-flex--basis-0",
     cardContainer: "u-margin--flexGap-double u-marginBlockStart--none md:u-displayFlex",
-    cardBox: "u-flow u-padding u-c--primary-min u-bg--grey-max u-border u-border--grey-med",
-    cardBoxLink: "u-width--100 u-displayFlex u-flex--stretchBlock u-textDecoration--none u-flow u-padding u-c--primary-min u-bg--grey-max u-border u-border--grey-med h:u-border--secondary-med h:u-innerShadow--secondary-med u-transition",
+    cardBox: "u-padding u-c--primary-min u-bg--grey-max u-border u-border--grey-med",
+    cardBoxLink: "h:u-border--secondary-med h:u-innerShadow--secondary-med",
     
     // Callout block
     callout: "u-posRelative u-paddingInline u-paddingBlockStart--double u-paddingBlockEnd",
