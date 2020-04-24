2686a491e619c28527ec4722c0f162fa9193e5a9
chriskirknielsen <chriskirknielsen@gmail.com>
2020-04-24 09:03:29 -0400

    Adjust footer copyright text

--- src/_data/footer.js
+++ src/_data/footer.js
@@ -11,6 +11,6 @@ module.exports = {
         langLabel: 'Langue',
         externalLinks: 'Retrouvez-moi sur ces autres sites',
         rssHint: 'S\'inscrire au flux RSS de chriskirknielsen',
-        buildInfo: 'Site construit avec <a href="https://www.11ty.dev" lang="en" aria-label="Eleventy" rel="noopener noreferrer" data-quiet-external-url>11ty</a>, hébergé sur <a href="https://netlify.com" rel="noopener noreferrer" data-quiet-external-url>Netlify</a>'
+        buildInfo: 'Site construit avec <a href="https://www.11ty.dev" lang="en" hreflang="en" aria-label="Eleventy" rel="noopener noreferrer" data-quiet-external-url>11ty</a>, hébergé sur <a href="https://netlify.com" lang="en" hreflang="en" rel="noopener noreferrer" data-quiet-external-url>Netlify</a>'
     }
 }
