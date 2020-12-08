a4c778b7a1e71e8e7efdfb20093afaaae16f2cb3
chriskirknielsen <chriskirknielsen@gmail.com>
2020-12-07 22:34:03 -0500

    Improve the DeLorean hook action script and viewBox resize "parameterisation"

--- src/_includes/assets/js/about.js
+++ src/_includes/assets/js/about.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     if (!aboutActions) { return; }
     var targetFrameRate = 60;
     var easeInOutQuad = function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t; } // @source https://gist.github.com/gre/1650294
-    var preciseNum = function(x) { return Number.parseFloat(x).toPrecision(4); }
-    var currentAnimatedValue = function (start, end, progress) {
+    var preciseNum = function(x) { return Number.parseFloat(x).toPrecision(4); } // Limit to four decimal places
+    var currentAnimatedValue = function (start, end, progress) { // Provide an eased value
         var eased = easeInOutQuad(progress);
         return preciseNum(start + (end - start) * eased);
     }
 
-    // Replace each hook with a usable button
+    // Replace each text hook with an actionable button
     for (var a = 0; a < aboutActions.length; a++) {
         var elementHtml = aboutActions[a].innerHTML;
         var elementHook = aboutActions[a].getAttribute(aboutActionAttr);
@@ -25,44 +25,57 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var a = 0; a < aboutActions.length; a++) {
         if (aboutActions[a].getAttribute(aboutActionAttr) === 'hooked-delorean') {
             var deLorean = document.querySelector('.about__delorean');
-            var initialClasses = deLorean.getAttribute('class');
-            var modifiedClass = 'about__delorean--with-hook';
-            var btn = aboutActions[a];
+            var w = parseFloat(deLorean.getAttribute('width')); // Get the initial width
+            var h = parseFloat(deLorean.getAttribute('height')); // Get the initial height
+            var initialClasses = deLorean.getAttribute('class'); // We can easily reference the classes later on with this
+            var modifiedClass = 'about__delorean--with-hook'; // This is a class to add when the state is toggled
+            var viewboxExpansion = deLorean.getAttribute('data-viewbox-expansion').trim(); // This contains the data about the new size of the SVG when toggled
+            var viewboxExpansionWH = viewboxExpansion.split(' '); // Split that data into the width and height values
+            var viewboxDirectionX = (viewboxExpansionWH[0] > 0) ? 1 : -1; // Whether the viewBox expands from the start or end of the horizontal dimension
+            var viewboxDirectionY = (viewboxExpansionWH[1] > 0) ? 1 : -1; // Whether the viewBox expands from the start or end of the vertical dimension
+            var viewboxExpansionW = Math.abs(parseFloat(viewboxExpansionWH[0])); // Get the absolute width change
+            var viewboxExpansionH = Math.abs(parseFloat(viewboxExpansionWH[1])); // Get the absolute height change
+            var btn = aboutActions[a]; // Assign the toggle trigger
             var animationDuration = 400; // Matches `$about-delorean-transition-duration` in src/_includes/assets/scss/pages/_about.scss
 
             var toggleViewBox = function() {
-                var w = parseFloat(deLorean.getAttribute('width'));
-                var h = parseFloat(deLorean.getAttribute('height'));
-                var isInitial = (deLorean.getAttribute('class').indexOf(modifiedClass) < 0);
-                var xOffset = 0.16;
-                var yOffset = 1.36;
+                var isInitial = (deLorean.getAttribute('class').indexOf(modifiedClass) < 0); // If the added class is not present, it is in its initial state
                 var animInterval = targetFrameRate / (animationDuration * 2); // The CSS transition needs to be faster, so this transition can be twice as long
-                var animProgress = 0;
+                var animProgress = 0; // Initialise the animation progress (belongs to [0,1])
                 
                 // Determine new state: if it is not in its initial state, return to initial state
-                var classes = initialClasses + ((!isInitial) ? '' : ' '+modifiedClass);
+                var classes = initialClasses + ((!isInitial) ? '' : ' '+modifiedClass); // Old school method for best browser compatibility
 
-                var startingX = (isInitial) ? 0 : w * xOffset * -1;
-                var startingY = (isInitial) ? 0 : h * yOffset * -1;
-                var startingW = (isInitial) ? w : w * (1 + xOffset);
-                var startingH = (isInitial) ? h : h * (1 + yOffset);
+                // Determine the starting value of each viewBox parameter
+                var startingX = (isInitial) ? 0 : viewboxExpansionW * viewboxDirectionX;
+                var startingY = (isInitial) ? 0 : viewboxExpansionH * viewboxDirectionY;
+                var startingW = (isInitial) ? w : w + viewboxExpansionW;
+                var startingH = (isInitial) ? h : h + viewboxExpansionH;
 
-                var targetX = (!isInitial) ? 0 : w * xOffset * -1;
-                var targetY = (!isInitial) ? 0 : h * yOffset * -1;
-                var targetW = (!isInitial) ? w : w * (1 + xOffset);
-                var targetH = (!isInitial) ? h : h * (1 + yOffset);
+                // Determine final resting value of each viewBox parameter
+                var targetX = (!isInitial) ? 0 : viewboxExpansionW * viewboxDirectionX;
+                var targetY = (!isInitial) ? 0 : viewboxExpansionH * viewboxDirectionY;
+                var targetW = (!isInitial) ? w : w + viewboxExpansionW;
+                var targetH = (!isInitial) ? h : h + viewboxExpansionH;
 
+                // Looping function to animate the viewBox values
                 var animateViewBox = function() {
-                    if (animProgress > 1) { return; }
+                    if (animProgress > 1) { // Enter the final resting state
+                        deLorean.setAttribute('viewBox', targetX + ' ' + targetY + ' ' + targetW + ' ' + targetH);
+                        return;
+                    }
 
+                    // Calculate an eased number for each of the four viewBox values
                     var currentX = currentAnimatedValue(startingX, targetX, animProgress);
                     var currentY = currentAnimatedValue(startingY, targetY, animProgress);
                     var currentW = currentAnimatedValue(startingW, targetW, animProgress);
                     var currentH = currentAnimatedValue(startingH, targetH, animProgress);
                     
+                    // Assign the final viewBox parameters as a single space-separated string
                     var newViewBox = currentX + ' ' + currentY + ' ' + currentW + ' ' + currentH;
                     deLorean.setAttribute('viewBox', newViewBox);
 
+                    // …and repeat
                     animProgress += animInterval;
                     requestAnimationFrame(animateViewBox);
                 }
