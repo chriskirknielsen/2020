ac1ba22228819de04d731cfdf1e95f70913c463c
chriskirknielsen <chriskirknielsen@gmail.com>
2021-01-16 19:01:23 -0500

    Add border-radius styles

--- src/_data/utilities.js
+++ src/_data/utilities.js
@@ -4,13 +4,13 @@ module.exports = { // Define a list of utility classes that are commonly used to
     
     // Global link with box hover effect
     boxLink: "u-fontBold u-textDecoration--none u-border--double u-border--accent u-bg--grey-max h:u-c--grey-max h:u-bg--accent",
-    glowBoxLink: "u-fontBold h:u-c--grey-min h:u-bg--grey-max u-border u-border--transparent h:u-border--accent h:u-glowBox--accent h:u-textShadow--currentBg h:u-textDecoration--none",
+    glowBoxLink: "u-fontBold h:u-c--grey-min h:u-bg--grey-max u-border u-border--transparent h:u-border--accent u-border-radius h:u-glowBox--accent h:u-textShadow--currentBg h:u-textDecoration--none",
     flexBoxLink: "u-displayInlineFlex u-flex--centerBlock u-padding",
 
     // Card component
     cardFlex: "u-flex--grow-1 u-flex--basis-0",
     cardContainer: "u-margin--flexGap-double u-marginBlockStart--none md:u-displayFlex",
-    cardBox: "u-padding u-c--text-color u-bg--grey-max u-border u-border--grey-med",
+    cardBox: "u-padding u-c--text-color u-bg--grey-max u-border u-border--grey-med u-border-radius",
     cardBoxLink: "h:u-border--accent h:u-glowBox--accent",
     
     // Callout block
