9a43b22ac1b5d53216af6e84d35de3020d1068e6
chriskirknielsen <chriskirknielsen@gmail.com>
2022-02-25 20:36:35 -0500

    Update shirt designs to include State of JS

--- src/_data/designs.js
+++ src/_data/designs.js
@@ -1,181 +1,192 @@
 module.exports = [
-    {
-        slug: 'retro-css',
-        name: 'Retro CSS',
-        description: 'A totally tubular t-shirt to show off how rad CSS is!',
-        img: 'retro-css.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/t-shirt/Retro-CSS-by-ckirknielsen/58081555.DYMRA',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/14125152-retro-css?store_id=49603',
-        },
-        tags: ['dev', 'css', 'retro']
-    },
-    {
-        slug: 'vhs-css',
-        name: 'Blank VHS CSS',
-        description: 'In a world where the web runs on VHS tapes…',
-        note: '(for posters, RedBubble or Society6 are recommended)',
-        img: 'vhs-css.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-CSS-by-ckirknielsen/72710420.6VDG0',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/19996533-retro-blank-vhs-css?store_id=49603',
-            'Society6': 'https://society6.com/product/retro-blank-vhs-css_t-shirt?sku=s6-21377318p15a4v136a5v20a11v49',
-        },
-        tags: ['dev', 'css', 'vhs', 'retro']
-    },
-    {
-        slug: 'vhs-html',
-        name: 'Blank VHS HTML',
-        description: 'In a world where the web runs on VHS tapes…',
-        note: '(for posters, RedBubble or Society6 are recommended)',
-        img: 'vhs-html.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-HTML-by-ckirknielsen/73662806.CW2C9.XYZ',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/20289562-retro-blank-vhs-html?store_id=49603',
-            'Society6': 'https://society6.com/product/retro-blank-vhs-html_t-shirt?sku=s6-21377261p15a4v97a5v18a11v49',
-        },
-        tags: ['dev', 'html', 'vhs', 'retro']
-    },
-    {
-        slug: 'vhs-svg',
-        name: 'Blank VHS SVG',
-        description: 'In a world where the web runs on VHS tapes…',
-        note: '(for posters, RedBubble or Society6 are recommended)',
-        img: 'vhs-svg.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-SVG-by-ckirknielsen/80893683.DYMRA',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/22687532-retro-blank-vhs-svg?store_id=49603',
-            'Society6': 'https://society6.com/product/retro-blank-vhs-svg_t-shirt?sku=s6-21377348p15a4v136a5v18a11v49',
-        },
-        tags: ['dev', 'svg', 'vhs', 'retro']
-    },
-    {
-        slug: 'vhs-js',
-        name: 'Blank VHS JS',
-        description: 'In a world where the web runs on VHS tapes…',
-        note: '(for posters, RedBubble or Society6 are recommended)',
-        img: 'vhs-js.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-JavaScript-ECMAScript-by-ckirknielsen/85189202.CW2C9.XYZ',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/23606086-retro-blank-vhs-javascript-ecmascript?store_id=49603',
-            'Society6': 'https://society6.com/product/retro-blank-vhs-javascript-ecmascript_t-shirt?sku=s6-21377364p15a4v97a5v18a11v49',
-        },
-        tags: ['dev', 'js', 'vhs', 'retro']
-    },
-    {
-        slug: 'css-fact-sheet',
-        name: 'CSS Fact Sheet',
-        description: 'A compact fact sheet for CSS that wouldn\'t be out of place in a cyberpunk world…',
-        img: 'css-fact-sheet.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/sticker/CSS-Fact-Sheet-by-ckirknielsen/58082119.EJUG5',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/14123881-css-fact-sheet?store_id=49603',
-        },
-        tags: ['dev', 'css']
-    },
-    {
-        slug: 'css-retro-vhs',
-        name: 'CSS Retro VHS',
-        description: 'Show your appreciation for everybody\'s favorite way to style the web with this retro shirt inspired by 90s VHS blank tapes',
-        note: 'Designed for the State of CSS survey, 2021 edition',
-        img: 'css-retro-vhs.jpg',
-        links: {
-            'CottonBureau': 'https://cottonbureau.com/products/css-retro-vhs-t-shirt#/13046404/tee-men-standard-tee-vintage-black-tri-blend-s'
-        },
-        tags: ['dev', 'css', 'vhs', 'retro']
-    },
-    {
-        slug: 'jak-daxter-powercell-hunters',
-        name: 'Jak & Daxter, Powercell Hunters',
-        description: 'A design inspired by one of my favourite video games: Jak And Daxter.',
-        img: 'powercell-hunters.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/t-shirt/Powercell-Hunters-by-ckirknielsen/84952593.2EZFS',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/23558909-powercell-hunters',
-        },
-        tags: ['video-games']
-    },
-    {
-        slug: 'crash-bandicoot-wumpa-hoarder',
-        name: 'Crash Bandicoot, Wumpa Hoarder',
-        description: 'A design inspired by the wild, Wumpa-loving bandicoot: Crash!',
-        img: 'wumpa-hoarder.jpg',
-        links: {
-            'RedBubble': 'https://www.redbubble.com/i/t-shirt/Wumpa-Hoarder-by-ckirknielsen/94462053.4AE2Y',
-            'TeePublic': 'https://www.teepublic.com/t-shirt/25571868-wumpa-hoarder',
-        },
-        tags: ['video-games']
-    },
-    {
-        slug: 'control-service-weapon',
-        name: 'Control, Service Weapon',
-        description: 'A poster showcasing a powerful handgun found in Control.',
-        img: 'control-service-weapon.jpg',
-        links: {
-            'Society6': 'https://society6.com/product/control-fbc-service-weapon_print?sku=s6-21852486p4a1v1',
-            'RedBubble': 'https://www.redbubble.com/i/poster/Control-FBC-Service-Weapon-by-ckirknielsen/89267833.LVTDI',
-            'TeePublic': 'https://www.teepublic.com/poster-and-art/24392079-control-fbc-service-weapon',
-        },
-        tags: ['video-games', 'gaming-arsenal']
-    },
-    {
-        slug: 'deathloop-fourpounder',
-        name: 'DEATHLOOP, Fourpounder',
-        description: 'A poster showcasing a stylish gun from DEATHLOOP.',
-        img: 'deathloop-fourpounder.jpg',
-        links: {
-            'Society6': 'https://society6.com/product/deathloop-fourpounder_print?sku=s6-22763844p4a1v45',
-            'RedBubble': 'https://www.redbubble.com/i/poster/DEATHLOOP-Fourpounder-by-ckirknielsen/97903910.LVTDI',
-            'TeePublic': 'https://www.teepublic.com/poster-and-art/26802077-deathloop-fourpounder',
-        },
-        tags: ['video-games', 'gaming-arsenal']
-    },
-    {
-        slug: 'god-of-war-leviathan-axe',
-        name: 'God of War, Leviathan Axe',
-        description: 'A poster showcasing a mythical axe from God of War.',
-        img: 'gow-axe.jpg',
-        links: {
-            'Society6': 'https://society6.com/product/god-of-war-leviathan-axe6077739_print?sku=s6-22068849p4a1v1',
-            'RedBubble': 'https://www.redbubble.com/i/poster/God-of-War-Leviathan-Axe-by-ckirknielsen/91430397.LVTDI',
-            'TeePublic': 'https://www.teepublic.com/poster-and-art/24830737-god-of-war-leviathan-axe',
-        },
-        tags: ['video-games', 'gaming-arsenal']
-    },
-    {
-        slug: 'god-of-war-lake-of-nine',
-        name: 'God of War, Lake of Nine',
-        description: 'A poster presenting a main area in God of War.',
-        img: 'gow-lake.jpg',
-        links: {
-            'Society6': 'https://society6.com/product/god-of-war-lake-of-nine_print?sku=s6-22069078p4a1v1',
-            'RedBubble': 'https://www.redbubble.com/i/poster/God-of-War-Lake-of-Nine-by-ckirknielsen/91430918.LVTDI',
-            'TeePublic': 'https://www.teepublic.com/poster-and-art/24831149-god-of-war-lake-of-nine',
-        },
-        tags: ['video-games']
-    },
-    {
-        slug: 'ratchet-clank-omniwrench',
-        name: 'Ratchet & Clank, Omniwrench',
-        description: 'A poster presenting a versatile tool in Ratchet & Clank.',
-        img: 'rc-wrench.jpg',
-        links: {
-            'Society6': 'https://society6.com/product/ratchet-clank-omniwrench-millenium_print?sku=s6-22239725p4a1v1',
-            'RedBubble': 'https://www.redbubble.com/i/poster/Ratchet-and-Clank-Omniwrench-Millenium-by-ckirknielsen/93357003.LVTDI',
-            'TeePublic': 'https://www.teepublic.com/poster-and-art/25205497-ratchet-and-clank-omniwrench-millenium',
-        },
-        tags: ['video-games', 'gaming-arsenal']
-    },
-    {
-        slug: 'dead-space-plasma-cutter',
-        name: 'Dead Space, Plasma Cutter',
-        description: 'A poster presenting an essential engineering tool in Dead Space.',
-        img: 'ds-plasma-cutter.jpg',
-        links: {
-            'Society6': 'https://society6.com/product/dead-space-plasma-cutter6489712_print?sku=s6-22965519p4a1v45',
-            'RedBubble': 'https://www.redbubble.com/i/poster/Dead-Space-Plasma-Cutter-by-ckirknielsen/99996925.LVTDI',
-            'TeePublic': 'https://www.teepublic.com/poster-and-art/27453494-dead-space-plasma-cutter',
-        },
-        tags: ['video-games', 'gaming-arsenal']
-    }
-]
+	{
+		slug: 'retro-css',
+		name: 'Retro CSS',
+		description: 'A totally tubular t-shirt to show off how rad CSS is!',
+		img: 'retro-css.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-CSS-by-ckirknielsen/58081555.DYMRA',
+			TeePublic: 'https://www.teepublic.com/t-shirt/14125152-retro-css?store_id=49603',
+		},
+		tags: ['dev', 'css', 'retro'],
+	},
+	{
+		slug: 'vhs-css',
+		name: 'Blank VHS CSS',
+		description: 'In a world where the web runs on VHS tapes…',
+		note: '(for posters, RedBubble or Society6 are recommended)',
+		img: 'vhs-css.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-CSS-by-ckirknielsen/72710420.6VDG0',
+			TeePublic: 'https://www.teepublic.com/t-shirt/19996533-retro-blank-vhs-css?store_id=49603',
+			Society6: 'https://society6.com/product/retro-blank-vhs-css_t-shirt?sku=s6-21377318p15a4v136a5v20a11v49',
+		},
+		tags: ['dev', 'css', 'vhs', 'retro'],
+	},
+	{
+		slug: 'vhs-html',
+		name: 'Blank VHS HTML',
+		description: 'In a world where the web runs on VHS tapes…',
+		note: '(for posters, RedBubble or Society6 are recommended)',
+		img: 'vhs-html.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-HTML-by-ckirknielsen/73662806.CW2C9.XYZ',
+			TeePublic: 'https://www.teepublic.com/t-shirt/20289562-retro-blank-vhs-html?store_id=49603',
+			Society6: 'https://society6.com/product/retro-blank-vhs-html_t-shirt?sku=s6-21377261p15a4v97a5v18a11v49',
+		},
+		tags: ['dev', 'html', 'vhs', 'retro'],
+	},
+	{
+		slug: 'vhs-svg',
+		name: 'Blank VHS SVG',
+		description: 'In a world where the web runs on VHS tapes…',
+		note: '(for posters, RedBubble or Society6 are recommended)',
+		img: 'vhs-svg.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-SVG-by-ckirknielsen/80893683.DYMRA',
+			TeePublic: 'https://www.teepublic.com/t-shirt/22687532-retro-blank-vhs-svg?store_id=49603',
+			Society6: 'https://society6.com/product/retro-blank-vhs-svg_t-shirt?sku=s6-21377348p15a4v136a5v18a11v49',
+		},
+		tags: ['dev', 'svg', 'vhs', 'retro'],
+	},
+	{
+		slug: 'vhs-js',
+		name: 'Blank VHS JS',
+		description: 'In a world where the web runs on VHS tapes…',
+		note: '(for posters, RedBubble or Society6 are recommended)',
+		img: 'vhs-js.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Retro-Blank-VHS-JavaScript-ECMAScript-by-ckirknielsen/85189202.CW2C9.XYZ',
+			TeePublic: 'https://www.teepublic.com/t-shirt/23606086-retro-blank-vhs-javascript-ecmascript?store_id=49603',
+			Society6: 'https://society6.com/product/retro-blank-vhs-javascript-ecmascript_t-shirt?sku=s6-21377364p15a4v97a5v18a11v49',
+		},
+		tags: ['dev', 'js', 'vhs', 'retro'],
+	},
+	{
+		slug: 'css-fact-sheet',
+		name: 'CSS Fact Sheet',
+		description: "A compact fact sheet for CSS that wouldn't be out of place on a crate in a cyberpunk world…",
+		img: 'css-fact-sheet.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/sticker/CSS-Fact-Sheet-by-ckirknielsen/58082119.EJUG5',
+			TeePublic: 'https://www.teepublic.com/t-shirt/14123881-css-fact-sheet?store_id=49603',
+		},
+		tags: ['dev', 'css'],
+	},
+	{
+		slug: 'css-retro-vhs',
+		name: 'CSS Retro VHS',
+		description: "Show your appreciation for everybody's favorite way to style the web with this retro shirt inspired by 90s VHS blank tapes",
+		note: 'Designed for the State of CSS survey, 2021 edition',
+		img: 'css-retro-vhs.jpg',
+		links: {
+			CottonBureau: 'https://cottonbureau.com/products/css-retro-vhs-t-shirt#/13046404/tee-men-standard-tee-vintage-black-tri-blend-s',
+		},
+		tags: ['dev', 'css', 'vhs', 'retro'],
+	},
+	{
+		slug: 'js-retro-vhs',
+		name: 'JS Retro VHS',
+		description: "Wear your JS colours from the perspective of a '90s VHS tape",
+		note: 'Designed for the State of JS survey, 2021 edition',
+		img: 'js-retro-vhs.jpg',
+		links: {
+			CottonBureau: 'https://cottonbureau.com/products/js-retro-vhs#/13799357/tee-men-standard-tee-vintage-black-tri-blend-s',
+		},
+		tags: ['dev', 'js', 'vhs', 'retro'],
+	},
+	{
+		slug: 'jak-daxter-powercell-hunters',
+		name: 'Jak & Daxter, Powercell Hunters',
+		description: 'A design inspired by one of my favourite video games: Jak And Daxter.',
+		img: 'powercell-hunters.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Powercell-Hunters-by-ckirknielsen/84952593.2EZFS',
+			TeePublic: 'https://www.teepublic.com/t-shirt/23558909-powercell-hunters',
+		},
+		tags: ['video-games'],
+	},
+	{
+		slug: 'crash-bandicoot-wumpa-hoarder',
+		name: 'Crash Bandicoot, Wumpa Hoarder',
+		description: 'A design inspired by the wild, Wumpa-loving bandicoot: Crash!',
+		img: 'wumpa-hoarder.jpg',
+		links: {
+			RedBubble: 'https://www.redbubble.com/i/t-shirt/Wumpa-Hoarder-by-ckirknielsen/94462053.4AE2Y',
+			TeePublic: 'https://www.teepublic.com/t-shirt/25571868-wumpa-hoarder',
+		},
+		tags: ['video-games'],
+	},
+	{
+		slug: 'control-service-weapon',
+		name: 'Control, Service Weapon',
+		description: 'A poster showcasing a powerful handgun found in Control.',
+		img: 'control-service-weapon.jpg',
+		links: {
+			Society6: 'https://society6.com/product/control-fbc-service-weapon_print?sku=s6-21852486p4a1v1',
+			RedBubble: 'https://www.redbubble.com/i/poster/Control-FBC-Service-Weapon-by-ckirknielsen/89267833.LVTDI',
+			TeePublic: 'https://www.teepublic.com/poster-and-art/24392079-control-fbc-service-weapon',
+		},
+		tags: ['video-games', 'gaming-arsenal'],
+	},
+	{
+		slug: 'deathloop-fourpounder',
+		name: 'DEATHLOOP, Fourpounder',
+		description: 'A poster showcasing a stylish gun from DEATHLOOP.',
+		img: 'deathloop-fourpounder.jpg',
+		links: {
+			Society6: 'https://society6.com/product/deathloop-fourpounder_print?sku=s6-22763844p4a1v45',
+			RedBubble: 'https://www.redbubble.com/i/poster/DEATHLOOP-Fourpounder-by-ckirknielsen/97903910.LVTDI',
+			TeePublic: 'https://www.teepublic.com/poster-and-art/26802077-deathloop-fourpounder',
+		},
+		tags: ['video-games', 'gaming-arsenal'],
+	},
+	{
+		slug: 'god-of-war-leviathan-axe',
+		name: 'God of War, Leviathan Axe',
+		description: 'A poster showcasing a mythical axe from God of War.',
+		img: 'gow-axe.jpg',
+		links: {
+			Society6: 'https://society6.com/product/god-of-war-leviathan-axe6077739_print?sku=s6-22068849p4a1v1',
+			RedBubble: 'https://www.redbubble.com/i/poster/God-of-War-Leviathan-Axe-by-ckirknielsen/91430397.LVTDI',
+			TeePublic: 'https://www.teepublic.com/poster-and-art/24830737-god-of-war-leviathan-axe',
+		},
+		tags: ['video-games', 'gaming-arsenal'],
+	},
+	{
+		slug: 'god-of-war-lake-of-nine',
+		name: 'God of War, Lake of Nine',
+		description: 'A poster presenting a main area in God of War.',
+		img: 'gow-lake.jpg',
+		links: {
+			Society6: 'https://society6.com/product/god-of-war-lake-of-nine_print?sku=s6-22069078p4a1v1',
+			RedBubble: 'https://www.redbubble.com/i/poster/God-of-War-Lake-of-Nine-by-ckirknielsen/91430918.LVTDI',
+			TeePublic: 'https://www.teepublic.com/poster-and-art/24831149-god-of-war-lake-of-nine',
+		},
+		tags: ['video-games'],
+	},
+	{
+		slug: 'ratchet-clank-omniwrench',
+		name: 'Ratchet & Clank, Omniwrench',
+		description: 'A poster presenting a versatile tool in Ratchet & Clank.',
+		img: 'rc-wrench.jpg',
+		links: {
+			Society6: 'https://society6.com/product/ratchet-clank-omniwrench-millenium_print?sku=s6-22239725p4a1v1',
+			RedBubble: 'https://www.redbubble.com/i/poster/Ratchet-and-Clank-Omniwrench-Millenium-by-ckirknielsen/93357003.LVTDI',
+			TeePublic: 'https://www.teepublic.com/poster-and-art/25205497-ratchet-and-clank-omniwrench-millenium',
+		},
+		tags: ['video-games', 'gaming-arsenal'],
+	},
+	{
+		slug: 'dead-space-plasma-cutter',
+		name: 'Dead Space, Plasma Cutter',
+		description: 'A poster presenting an essential engineering tool in Dead Space.',
+		img: 'ds-plasma-cutter.jpg',
+		links: {
+			Society6: 'https://society6.com/product/dead-space-plasma-cutter6489712_print?sku=s6-22965519p4a1v45',
+			RedBubble: 'https://www.redbubble.com/i/poster/Dead-Space-Plasma-Cutter-by-ckirknielsen/99996925.LVTDI',
+			TeePublic: 'https://www.teepublic.com/poster-and-art/27453494-dead-space-plasma-cutter',
+		},
+		tags: ['video-games', 'gaming-arsenal'],
+	},
+];
