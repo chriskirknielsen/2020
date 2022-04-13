34b8cd2cac5549724f5f1ecdbcf5339e67136006
chriskirknielsen <chriskirknielsen@gmail.com>
2022-04-12 22:49:00 -0400

    Reformat 11ty config

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,72 +1,73 @@
 module.exports = {
-  title: "chriskirknielsen",
-  url: "https://chriskirknielsen.com",
-  repo: "https://github.com/chriskirknielsen/2020",
-  author: {
-    name: "Christopher Kirk-Nielsen",
-    email: "chriskirknielsen@gmail.com",
-    twitter: "https://twitter.com/ckirknielsen",
-  },
-  copyrightStart: 2019,
-  currentYear: (new Date()).getFullYear(),
-  primaryLanguage: "en",
-  navSet: ["home", "blog", "about"],
-  navGlobalPages: ["designs"],
-  languages: {
-    "en": {
-      label: "English",
-      path: "/",
-      root: "",
-    },
-    "fr": {
-      label: "Français",
-      path: "/fr",
-      root: "fr/",
-    }
-  },
-  merch: {
-    "TeePublic": "https://www.teepublic.com/user/chriskirknielsen/albums/113954-web-development",
-    "RedBubble": "https://www.redbubble.com/people/ckirknielsen/shop",
-    "Society6": "https://society6.com/chriskirknielsen/prints"
-  },
-  support: {
-    "kofi": "https://ko-fi.com/chriskirknielsen",
-    "paypal": "https://paypal.me/ckn92usd?locale.x=en_US"
-  },
-  assetUrl: {
-    manifest: "manifest.json",
-    authorAvatar: "assets/img/profile.jpg",
-    icons: {
-      faviconPng: "assets/img/icon.png",
-      faviconSvg: "assets/img/favicon.svg",
-      maskIcon: "assets/img/mask-icon.svg",
-      appleTouchIcon: "assets/img/apple-touch-icon.png",
-      googleIcon: "assets/img/google-touch-icon.png",
-    },
-    fonts: {
-      heading: "assets/fonts/MarvinVisionsBig-Bold.woff2"
-    },
-    scriptsFolder: "assets/jsmin",
-    stylesFolder: "assets/css",
-    allStyles: "assets/css/style.css",
-    inlineStyles: "assets/css/inline.css",
-    globalStyles: "assets/css/global.css",
-    imagesFolder: "assets/img",
-    designsFolder: "assets/img/designs",
-    fontFaceFolder: "assets/fonts",
-    fontFaceStyles: "assets/css/font-face.css",
-  },
-  cloudinary: {
-    user: 'chriskirknielsen',
-    url: 'https://res.cloudinary.com/chriskirknielsen',
-    logo: 'ckn_logo',
-    image: 'ckn_post_bg',
-    font: 'MarvinVisionsBigBold.otf',
-    fontSize: 100,
-    outlineSize: 12,
-    bg: '#1d122f',
-    color: '#ffe4c2',
-    tint: '#ff0196',
-  },
-  showPostAuthor: true
-}
+	title: 'chriskirknielsen',
+	url: 'https://chriskirknielsen.com',
+	repo: 'https://github.com/chriskirknielsen/2020',
+	author: {
+		name: 'Christopher Kirk-Nielsen',
+		email: 'chriskirknielsen@gmail.com',
+		twitter: 'https://twitter.com/ckirknielsen',
+	},
+	copyrightStart: 2019,
+	currentYear: new Date().getFullYear(),
+	primaryLanguage: 'en',
+	navSet: ['home', 'blog', 'about'],
+	navGlobalPages: ['designs'],
+	languages: {
+		en: {
+			label: 'English',
+			path: '/',
+			root: '',
+		},
+		fr: {
+			label: 'Français',
+			path: '/fr',
+			root: 'fr/',
+		},
+	},
+	merch: {
+		TeePublic: 'https://www.teepublic.com/user/chriskirknielsen/albums/113954-web-development',
+		RedBubble: 'https://www.redbubble.com/people/ckirknielsen/shop',
+		Society6: 'https://society6.com/chriskirknielsen/prints',
+		DesignByHumans: 'https://www.designbyhumans.com/shop/chriskirknielsen/',
+	},
+	support: {
+		kofi: 'https://ko-fi.com/chriskirknielsen',
+		paypal: 'https://paypal.me/ckn92usd?locale.x=en_US',
+	},
+	assetUrl: {
+		manifest: 'manifest.json',
+		authorAvatar: 'assets/img/profile.jpg',
+		icons: {
+			faviconPng: 'assets/img/icon.png',
+			faviconSvg: 'assets/img/favicon.svg',
+			maskIcon: 'assets/img/mask-icon.svg',
+			appleTouchIcon: 'assets/img/apple-touch-icon.png',
+			googleIcon: 'assets/img/google-touch-icon.png',
+		},
+		fonts: {
+			heading: 'assets/fonts/MarvinVisionsBig-Bold.woff2',
+		},
+		scriptsFolder: 'assets/jsmin',
+		stylesFolder: 'assets/css',
+		allStyles: 'assets/css/style.css',
+		inlineStyles: 'assets/css/inline.css',
+		globalStyles: 'assets/css/global.css',
+		imagesFolder: 'assets/img',
+		designsFolder: 'assets/img/designs',
+		fontFaceFolder: 'assets/fonts',
+		fontFaceStyles: 'assets/css/font-face.css',
+	},
+	cloudinary: {
+		user: 'chriskirknielsen',
+		url: 'https://res.cloudinary.com/chriskirknielsen',
+		logo: 'ckn_logo',
+		image: 'ckn_post_bg',
+		font: 'MarvinVisionsBigBold.otf',
+		fontSize: 100,
+		outlineSize: 12,
+		bg: '#1d122f',
+		color: '#ffe4c2',
+		tint: '#ff0196',
+	},
+	showPostAuthor: true,
+};
